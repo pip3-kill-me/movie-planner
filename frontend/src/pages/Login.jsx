ac40e48507683d25136169621f15530d00d3575d
plets <mastheuss@gmail.com>
2025-11-08 04:45:35 -0300

    credentials structure api

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
-import { API_URL } from "./lib/api";
+import { API_URL } from "../lib/api";
 
 export default function Login({ onLogin }) {
   const [username, setUsername] = useState("");
